be18b061a107ed0740fc400a1185f37cd2d99c55
lutherx21 <77136757+lutherx21@users.noreply.github.com>
2022-04-28 06:56:07 -0700

    Add files via upload

---
+++ ageIdentifier/Backup/ageIdentifier/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DTPdateBIRTH = New System.Windows.Forms.DateTimePicker
+        Me.DTPendDATE = New System.Windows.Forms.DateTimePicker
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'DTPdateBIRTH
+        '
+        Me.DTPdateBIRTH.Location = New System.Drawing.Point(108, 47)
+        Me.DTPdateBIRTH.Name = "DTPdateBIRTH"
+        Me.DTPdateBIRTH.Size = New System.Drawing.Size(286, 20)
+        Me.DTPdateBIRTH.TabIndex = 0
+        '
+        'DTPendDATE
+        '
+        Me.DTPendDATE.Location = New System.Drawing.Point(108, 84)
+        Me.DTPendDATE.Name = "DTPendDATE"
+        Me.DTPendDATE.Size = New System.Drawing.Size(286, 20)
+        Me.DTPendDATE.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Date of Birth :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "End of date :"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(14, 143)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(383, 76)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(308, 108)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 21)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(40, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(305, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Calculate the Age with Zodiac Sign"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.ClientSize = New System.Drawing.Size(409, 224)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DTPendDATE)
+        Me.Controls.Add(Me.DTPdateBIRTH)
+        Me.Font = New System.Drawing.Font("Arial Rounded MT Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DTPdateBIRTH As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DTPendDATE As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ ageIdentifier/ageIdentifier/Form1.vb
@@ -0,0 +1,105 @@
+﻿Public Class Form1
+    
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'SET AN ARRAY VARIABLE TO STORE THE VALUE OF THE ZODIAC SIGNS ON IT.
+        Dim zodiacSign() As String = {"Aries", "Taurus", "Gemini", "Cancer", _
+                                  "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", _
+                                  "Capricorn", "Aquarius", "Pisces"}
+
+        'SET A VARIABLE TO REPRESENTS YOUR AGE.
+        Dim AGE As Integer
+        'SET THE STRING VARIABLE REPRESENT A FORMATED DATETIMEPICKER TO THE NAME OF THE MONTHS.
+        Dim MONTHS As String = Format(DTPdateBIRTH.Value, "MMMM")
+        'SET THE INTEGR VARIABLE REPRESENT A FORMATED DATETIMEPICKER TO A VALUE OF THE DAY.
+        Dim DAYS As Integer = Format(DTPdateBIRTH.Value, "dd")
+
+        'CALCULATING THE INTERVAL BETWEEN THE DATE OF BIRTH AND THE END OF THE DATE.
+        age = DateDiff(DateInterval.Year, DTPdateBIRTH.Value, DTPendDATE.Value)
+        'CLEARING THE LISTBOX.
+        ListBox1.Items.Clear()
+        'ADDING THE VALUE OF THE AGE IN THE LISTBOX TAHT YOU HAVE CALCULATED.
+        ListBox1.Items.Add("Age : " & age).ToString()
+        'YOU HAVE TO PUT A CONDITION IN ORDER TO SET THE SPECIFIC ZODIAC ZIGN OF THE DAYS IN THE MONTHS.
+        Select Case MONTHS
+            Case "January"
+                If DAYS >= 1 And DAYS <= 19 Then
+                    'ADDING A ZODIAC SIGN IN THE LIST BOXS.
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(9))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(10))
+                End If
+            Case "February"
+                If DAYS >= 1 And DAYS <= 18 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(10))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(11))
+                End If
+            Case "March"
+                If DAYS >= 21 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(0))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(11))
+                End If
+            Case "April"
+                If DAYS >= 1 And DAYS <= 19 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(0))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(1))
+                End If
+            Case "May"
+                If DAYS >= 1 And DAYS <= 20 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(1))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(2))
+                End If
+            Case "June"
+                If DAYS >= 1 And DAYS <= 21 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(2))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(3))
+                End If
+            Case "July"
+                If DAYS >= 1 And DAYS <= 22 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(3))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(4))
+                End If
+            Case "August"
+                If DAYS >= 1 And DAYS <= 22 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(4))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(5))
+                End If
+            Case "September"
+                If DAYS >= 1 And DAYS <= 22 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(5))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(6))
+                End If
+            Case "October"
+                If DAYS >= 1 And DAYS <= 22 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(6))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(7))
+                End If
+            Case "November"
+                If DAYS >= 1 And DAYS <= 21 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(7))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(8))
+                End If
+            Case "December"
+                If DAYS >= 1 And DAYS <= 21 Then
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(8))
+                Else
+                    ListBox1.Items.Add("Zodiac Sign :" & zodiacSign(9))
+                End If
+        End Select
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
